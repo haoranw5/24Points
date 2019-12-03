be92383a0ea33a350c428339f7feb7be4e56d733
Haoran Wang <haoranw5@illinois.edu>
2019-12-03 10:07:26 -0600

    3

--- app/src/main/java/com/example/a24_points/MainActivity.java
+++ app/src/main/java/com/example/a24_points/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
